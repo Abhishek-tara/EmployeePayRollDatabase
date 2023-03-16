941e84be4e614a1495dff16c32f3e51910d8c9ee
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-16 21:35:55 +0530

    Created PayRoll Table

--- PayRollDB.sql
+++ PayRollDB.sql
@@ -1,3 +1,12 @@
 -- Welcome to EmployeePayroll Problem
 --     UC-01-CreateDatabase
 create database Payroll 
+
+-- UC-02 Create Employee_Payroll Table
+create Table EmployeePayroll
+(
+id int identity primary key,
+name varchar(200) not null,
+salary money not null,
+start_Date date not null
+)
