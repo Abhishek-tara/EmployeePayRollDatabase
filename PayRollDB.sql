5daea784ce7197405ba5405a3daf20d440faedc3
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-03-16 21:42:57 +0530

    Retreving Specific data

--- PayRollDB.sql
+++ PayRollDB.sql
@@ -15,10 +15,17 @@ start_Date date not null
 
 --UC-3 Insert values in Table
 Insert into EmployeePayroll values
-('Rinku',20000,'2023-01-12'),
+('Abhi',20000,'2023-01-12'),
 ('Swayam',25000,'2020-02-18'),
 ('Arya',10000,'2022-11-13'),
 ('Gitesh',30000,'2019-06-09')
 
 --UC-4 Retrieve All data--
 select * from EmployeePayroll
+
+
+-- UC5 Retrieve salary for particular employee 
+select salary from EmployeePayroll where name='Abhi'
+
+-- Retrieve salary for particular employee who join in particular date using getdate()
+select salary from  EmployeePayroll where start_Date between '2020-01-12' and getdate()
